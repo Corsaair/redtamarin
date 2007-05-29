d7f3d54e707ac7d29b35b30624089fc2b52eec1e
zwetan <zwetan@gmail.com>
2007-05-29 15:02:09 +0000

    update to copy both binaries
    git-svn-id: http://dev.corsaair.com/svn/redtamarin/trunk@19 8266667d-5eeb-4550-8638-74b212e0aa7d

--- copyBinaries.bat
+++ copyBinaries.bat
@@ -1,2 +1,3 @@
 @echo off
 xcopy mozilla\js\tamarin\platform\win32\obj_8\shell\Debug_Debugger\avmplus_sd.exe bin\*.* /Y
+xcopy mozilla\js\tamarin\platform\win32\obj_8\shell\Release\avmplus.exe bin\*.* /Y
