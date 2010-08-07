ca9d942ffb18f3c3ec116c27c36f1c7490d51b7d
zwetan <zwetan@gmail.com>
2010-08-07 12:25:52 +0000

    update
    git-svn-id: http://dev.corsaair.com/svn/redtamarin/trunk@389 8266667d-5eeb-4550-8638-74b212e0aa7d

--- shell/shell_toplevel.h
+++ shell/shell_toplevel.h
@@ -65,7 +65,7 @@ namespace avmplus {
     class NewObjectSampleObject; //flash.sampler::NewObjectSample
     class SampleClass; //flash.sampler::Sample$
     class SampleObject; //flash.sampler::Sample
-    class ScriptObject; //C.stdlib::__stdlib
+    class ScriptObject; //avmplus::File
     class StackFrameClass; //flash.sampler::StackFrame$
     class StackFrameObject; //flash.sampler::StackFrame
     class String; //String
