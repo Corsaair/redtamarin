7c95f82a8f437b23b8496ebd48fecb49f14a8e9c
zwetan <zwetan@gmail.com>
2010-12-28 14:48:34 +0000

    removing SELFTEST by default, we don't want or need them
    git-svn-id: http://dev.corsaair.com/svn/redtamarin/trunk@651 8266667d-5eeb-4550-8638-74b212e0aa7d

--- shell/avmshell-features.h
+++ shell/avmshell-features.h
@@ -110,7 +110,7 @@
 #endif
 
 #ifndef AVMFEATURE_SELFTEST
-  #define AVMFEATURE_SELFTEST          1
+  #define AVMFEATURE_SELFTEST          0
 #endif
 
 // https://bugzilla.mozilla.org/show_bug.cgi?id=491866
