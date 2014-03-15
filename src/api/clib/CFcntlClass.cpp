7a1c7e5fdf727d5480e641f5831826b4a15cafd2
zwetan <zwetan@gmail.com>
2014-03-15 17:04:04 +0000

    updating fcntl implementation
    git-svn-id: http://dev.corsaair.com/svn/redtamarin/trunk@1471 8266667d-5eeb-4550-8638-74b212e0aa7d

--- src/api/clib/CFcntlClass.cpp
+++ src/api/clib/CFcntlClass.cpp
@@ -31,8 +31,45 @@ namespace avmshell
     int CFcntlClass::get_O_TRUNC() { return O_TRUNC; }
     int CFcntlClass::get_O_EXCL() { return O_EXCL; }
 
+    int CFcntlClass::get_O_EXEC() { return O_EXEC; }
+    int CFcntlClass::get_O_CLOEXEC() { return O_CLOEXEC; }
+    int CFcntlClass::get_O_DIRECTORY() { return O_DIRECTORY; }
+    int CFcntlClass::get_O_NOFOLLOW() { return O_NOFOLLOW; }
+    int CFcntlClass::get_O_NONBLOCK() { return O_NONBLOCK; }
+    int CFcntlClass::get_O_NOCTTY() { return O_NOCTTY; }
+    int CFcntlClass::get_O_SEARCH() { return O_SEARCH; }
+    int CFcntlClass::get_O_TTY_INIT() { return O_TTY_INIT; }
+
+    int CFcntlClass::get_O_SYNC() { return O_SYNC; }
+    int CFcntlClass::get_O_DSYNC() { return O_DSYNC; }
+    int CFcntlClass::get_O_RSYNC() { return O_RSYNC; }
+    int CFcntlClass::get_O_ASYNC() { return O_ASYNC; }
+    int CFcntlClass::get_O_FSYNC() { return O_FSYNC; }
+
+    int CFcntlClass::get_O_SHLOCK() { return O_SHLOCK; }
+    int CFcntlClass::get_O_EXLOCK() { return O_EXLOCK; }
+
     int CFcntlClass::get_O_TEXT() { return O_TEXT; }
     int CFcntlClass::get_O_BINARY() { return O_BINARY; }
+    int CFcntlClass::get_O_WTEXT() { return O_WTEXT; }
+    int CFcntlClass::get_O_U16TEXT() { return O_U16TEXT; }
+    int CFcntlClass::get_O_U8TEXT() { return O_U8TEXT; }
+    int CFcntlClass::get_O_RAW() { return O_RAW; }
+    int CFcntlClass::get_O_NOINHERIT() { return O_NOINHERIT; }
+    int CFcntlClass::get_O_TEMPORARY() { return O_TEMPORARY; }
+    int CFcntlClass::get_O_SHORT_LIVED() { return O_SHORT_LIVED; }
+    int CFcntlClass::get_O_SEQUENTIAL() { return O_SEQUENTIAL; }
+    int CFcntlClass::get_O_RANDOM() { return O_RANDOM; }
+
+
+    int CFcntlClass::get_F_DUPFD() { return F_DUPFD; }
+    int CFcntlClass::get_F_DUPFD_CLOEXEC() { return F_DUPFD_CLOEXEC; }
+    int CFcntlClass::get_F_GETFD() { return F_GETFD; }
+    int CFcntlClass::get_F_SETFD() { return F_SETFD; }
+    int CFcntlClass::get_F_GETFL() { return F_GETFL; }
+    int CFcntlClass::get_F_SETFL() { return F_SETFL; }
+    int CFcntlClass::get_F_GETOWN() { return F_GETOWN; }
+    int CFcntlClass::get_F_SETOWN() { return F_SETOWN; }
 
 
     /*static*/ int CFcntlClass::creat(ScriptObject* self, Stringp path, int mode)
@@ -48,6 +85,11 @@ namespace avmshell
         return VMPI_creat( pathUTF8.c_str(), mode );
     }
 
+    /*static*/ int CFcntlClass::fcntl(ScriptObject*, int fildes, int cmd, int arg)
+    {
+        return VMPI_fcntl( fildes, cmd, arg );
+    }
+
     /*static*/ int CFcntlClass::open(ScriptObject* self, Stringp path, int oflag, int mode)
     {
         Toplevel* toplevel = self->toplevel();
@@ -58,7 +100,28 @@ namespace avmshell
         }
         
         StUTF8String pathUTF8(path);
-        return VMPI_open( pathUTF8.c_str(), oflag, mode );
+
+        if( mode > -1 )
+        {
+            return VMPI_open( pathUTF8.c_str(), oflag, mode );    
+        }
+        else
+        {
+            return VMPI_open( pathUTF8.c_str(), oflag );
+        }
+    }
+
+    /*static*/ int CFcntlClass::openat(ScriptObject* self, int fd, Stringp path, int oflag, int mode)
+    {
+        Toplevel* toplevel = self->toplevel();
+
+        if( !path )
+        {
+            toplevel->throwArgumentError(kNullArgumentError, "path");
+        }
+        
+        StUTF8String pathUTF8(path);
+        return VMPI_openat( fd, pathUTF8.c_str(), oflag, mode );
     }
 
 }
--- src/api/clib/CFcntlClass.h
+++ src/api/clib/CFcntlClass.h
@@ -26,13 +26,52 @@ namespace avmshell
         int get_O_CREAT();
         int get_O_TRUNC();
         int get_O_EXCL();
+
+        int get_O_EXEC();
+        int get_O_CLOEXEC();
+        int get_O_DIRECTORY();
+        int get_O_NOFOLLOW();
+        int get_O_NONBLOCK();
+        int get_O_NOCTTY();
+        int get_O_SEARCH();
+        int get_O_TTY_INIT();
+
+        int get_O_SYNC();
+        int get_O_DSYNC();
+        int get_O_RSYNC();
+        int get_O_ASYNC();
+        int get_O_FSYNC();
+
+        int get_O_SHLOCK();
+        int get_O_EXLOCK();
         
         int get_O_TEXT();
         int get_O_BINARY();
+        int get_O_WTEXT();
+        int get_O_U16TEXT();
+        int get_O_U8TEXT();
+        int get_O_RAW();
+        int get_O_NOINHERIT();
+        int get_O_TEMPORARY();
+        int get_O_SHORT_LIVED();
+        int get_O_SEQUENTIAL();
+        int get_O_RANDOM();
+
+
+        int get_F_DUPFD();
+        int get_F_DUPFD_CLOEXEC();
+        int get_F_GETFD();
+        int get_F_SETFD();
+        int get_F_GETFL();
+        int get_F_SETFL();
+        int get_F_GETOWN();
+        int get_F_SETOWN();
 
 
         static int creat(ScriptObject* self, Stringp path, int mode);
+        static int fcntl(ScriptObject*, int fildes, int cmd, int arg);
         static int open(ScriptObject* self, Stringp path, int oflag, int mode);
+        static int openat(ScriptObject* self, int fd, Stringp path, int oflag, int mode);
         
         DECLARE_SLOTS_CFcntlClass;
     };
