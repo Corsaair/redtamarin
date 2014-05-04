c644f6b72b8319cdb5b0d3d5e2d8212a687f6449
zwetan <zwetan@gmail.com>
2014-05-04 21:58:47 +0000

    limits.h definitions
    git-svn-id: http://dev.corsaair.com/svn/redtamarin/trunk@1490 8266667d-5eeb-4550-8638-74b212e0aa7d

--- src/api/clib/CLimitsClass.cpp
+++ src/api/clib/CLimitsClass.cpp
@@ -20,6 +20,109 @@ namespace avmshell
         
     }
 
+    int CLimitsClass::get_CHAR_BIT() { return CHAR_BIT; }
+    int CLimitsClass::get_CHAR_MAX() { return CHAR_MAX; }
+    int CLimitsClass::get_CHAR_MIN() { return CHAR_MIN; }
+    int CLimitsClass::get_INT_MAX() { return INT_MAX; }
+    int CLimitsClass::get_INT_MIN() { return INT_MIN; }
+    double CLimitsClass::get_LLONG_MAX() { return LLONG_MAX; }
+    double CLimitsClass::get_LLONG_MIN() { return LLONG_MIN; }
+    int CLimitsClass::get_LONG_BIT() { return LONG_BIT; }
+    int CLimitsClass::get_LONG_MAX() { return LONG_MAX; }
+    int CLimitsClass::get_LONG_MIN() { return LONG_MIN; }
+    int CLimitsClass::get_SCHAR_MAX() { return SCHAR_MAX; }
+    int CLimitsClass::get_SCHAR_MIN() { return SCHAR_MIN; }
+    int CLimitsClass::get_SHRT_MAX() { return SHRT_MAX; }
+    int CLimitsClass::get_SHRT_MIN() { return SHRT_MIN; }
+    int CLimitsClass::get_UCHAR_MAX() { return UCHAR_MAX; }
+    uint32_t CLimitsClass::get_UINT_MAX() { return UINT_MAX; }
+    double CLimitsClass::get_ULLONG_MAX() { return ULLONG_MAX; }
+    uint32_t CLimitsClass::get_ULONG_MAX() { return ULONG_MAX; }
+    int CLimitsClass::get_USHRT_MAX() { return USHRT_MAX; }
+    int CLimitsClass::get_WORD_BIT() { return WORD_BIT; }
+
+    int CLimitsClass::get_ARG_MAX() { return ARG_MAX; }
+    int CLimitsClass::get_ATEXIT_MAX() { return ATEXIT_MAX; }
+    int CLimitsClass::get_CHILD_MAX() { return CHILD_MAX; }
+    int CLimitsClass::get_LINK_MAX() { return LINK_MAX; }
+    int CLimitsClass::get_MAX_CANON() { return MAX_CANON; }
+    int CLimitsClass::get_MAX_INPUT() { return MAX_INPUT; }
+    int CLimitsClass::get_NAME_MAX() { return NAME_MAX; }
+    int CLimitsClass::get_NGROUPS_MAX() { return NGROUPS_MAX; }
+    int CLimitsClass::get_OPEN_MAX() { return OPEN_MAX; }
     int CLimitsClass::get_PATH_MAX() { return PATH_MAX; }
+    int CLimitsClass::get_PIPE_BUF() { return PIPE_BUF; }
+    int CLimitsClass::get_SSIZE_MAX() { return SSIZE_MAX; }
+    int CLimitsClass::get_STREAM_MAX() { return STREAM_MAX; }
+    int CLimitsClass::get_TZNAME_MAX() { return TZNAME_MAX; }
+
+    int CLimitsClass::get_MB_LEN_MAX() { return MB_LEN_MAX; }
+    int CLimitsClass::get_SIZE_MAX() { return SIZE_MAX; }
+    int CLimitsClass::get_SYMLINK_MAX() { return SYMLINK_MAX; }
+
+    int CLimitsClass::get_AIO_LISTIO_MAX() { return AIO_LISTIO_MAX; }
+    int CLimitsClass::get_AIO_MAX() { return AIO_MAX; }
+    int CLimitsClass::get_AIO_PRIO_DELTA_MAX() { return AIO_PRIO_DELTA_MAX; }
+    int CLimitsClass::get_DELAYTIMER_MAX() { return DELAYTIMER_MAX; }
+    int CLimitsClass::get_HOST_NAME_MAX() { return HOST_NAME_MAX; }
+    int CLimitsClass::get_LOGIN_NAME_MAX() { return LOGIN_NAME_MAX; }
+    int CLimitsClass::get_MQ_OPEN_MAX() { return MQ_OPEN_MAX; }
+    int CLimitsClass::get_MQ_PRIO_MAX() { return MQ_PRIO_MAX; }
+
+    int CLimitsClass::get_PAGESIZE() { return PAGESIZE; }
+    int CLimitsClass::get_PAGE_SIZE() { return PAGE_SIZE; }
+    int CLimitsClass::get_RE_DUP_MAX() { return RE_DUP_MAX; }
+    int CLimitsClass::get_RTSIG_MAX() { return RTSIG_MAX; }
+    int CLimitsClass::get_SEM_NSEMS_MAX() { return SEM_NSEMS_MAX; }
+    int CLimitsClass::get_SEM_VALUE_MAX() { return SEM_VALUE_MAX; }
+    int CLimitsClass::get_SIGQUEUE_MAX() { return SIGQUEUE_MAX; }
+    int CLimitsClass::get_SS_REPL_MAX() { return SS_REPL_MAX; }
+
+    int CLimitsClass::get_SYMLOOP_MAX() { return SYMLOOP_MAX; }
+    int CLimitsClass::get_TIMER_MAX() { return TIMER_MAX; }
+    int CLimitsClass::get_TRACE_EVENT_NAME_MAX() { return TRACE_EVENT_NAME_MAX; }
+    int CLimitsClass::get_TRACE_NAME_MAX() { return TRACE_NAME_MAX; }
+    int CLimitsClass::get_TRACE_SYS_MAX() { return TRACE_SYS_MAX; }
+    int CLimitsClass::get_TRACE_USER_EVENT_MAX() { return TRACE_USER_EVENT_MAX; }
+    int CLimitsClass::get_TTY_NAME_MAX() { return TTY_NAME_MAX; }
+
+    int CLimitsClass::get__POSIX_ARG_MAX() { return _POSIX_ARG_MAX; }
+    int CLimitsClass::get__POSIX_CHILD_MAX() { return _POSIX_CHILD_MAX; }
+    int CLimitsClass::get__POSIX_LINK_MAX() { return _POSIX_LINK_MAX; }
+    int CLimitsClass::get__POSIX_MAX_CANON() { return _POSIX_MAX_CANON; }
+    int CLimitsClass::get__POSIX_MAX_INPUT() { return _POSIX_MAX_INPUT; }
+    int CLimitsClass::get__POSIX_NAME_MAX() { return _POSIX_NAME_MAX; }
+	int CLimitsClass::get__POSIX_NGROUPS_MAX() { return _POSIX_NGROUPS_MAX; }
+	int CLimitsClass::get__POSIX_OPEN_MAX() { return _POSIX_OPEN_MAX; }
+	int CLimitsClass::get__POSIX_PATH_MAX() { return _POSIX_PATH_MAX; }
+	int CLimitsClass::get__POSIX_PIPE_BUF() { return _POSIX_PIPE_BUF; }
+	int CLimitsClass::get__POSIX_SSIZE_MAX() { return _POSIX_SSIZE_MAX; }
+	int CLimitsClass::get__POSIX_STREAM_MAX() { return _POSIX_STREAM_MAX; }
+	int CLimitsClass::get__POSIX_TZNAME_MAX() { return _POSIX_TZNAME_MAX; }
+	int CLimitsClass::get__POSIX_SYMLINK_MAX() { return _POSIX_SYMLINK_MAX; }
+
+	int CLimitsClass::get__POSIX_AIO_LISTIO_MAX() { return _POSIX_AIO_LISTIO_MAX; }
+	int CLimitsClass::get__POSIX_AIO_MAX() { return _POSIX_AIO_MAX; }
+	int CLimitsClass::get__POSIX_DELAYTIMER_MAX() { return _POSIX_DELAYTIMER_MAX; }
+	int CLimitsClass::get__POSIX_HOST_NAME_MAX() { return _POSIX_HOST_NAME_MAX; }
+	int CLimitsClass::get__POSIX_LOGIN_NAME_MAX() { return _POSIX_LOGIN_NAME_MAX; }
+	int CLimitsClass::get__POSIX_MQ_OPEN_MAX() { return _POSIX_MQ_OPEN_MAX; }
+	int CLimitsClass::get__POSIX_MQ_PRIO_MAX() { return _POSIX_MQ_PRIO_MAX; }
+
+	int CLimitsClass::get__POSIX_RE_DUP_MAX() { return _POSIX_RE_DUP_MAX; }
+	int CLimitsClass::get__POSIX_RTSIG_MAX() { return _POSIX_RTSIG_MAX; }
+	int CLimitsClass::get__POSIX_SEM_NSEMS_MAX() { return _POSIX_SEM_NSEMS_MAX; }
+	int CLimitsClass::get__POSIX_SEM_VALUE_MAX() { return _POSIX_SEM_VALUE_MAX; }
+	int CLimitsClass::get__POSIX_SIGQUEUE_MAX() { return _POSIX_SIGQUEUE_MAX; }
+	int CLimitsClass::get__POSIX_SS_REPL_MAX() { return _POSIX_SS_REPL_MAX; }
+
+	int CLimitsClass::get__POSIX_SYMLOOP_MAX() { return _POSIX_SYMLOOP_MAX; }
+	int CLimitsClass::get__POSIX_TIMER_MAX() { return _POSIX_TIMER_MAX; }
+	int CLimitsClass::get__POSIX_TRACE_EVENT_NAME_MAX() { return _POSIX_TRACE_EVENT_NAME_MAX; }
+	int CLimitsClass::get__POSIX_TRACE_NAME_MAX() { return _POSIX_TRACE_NAME_MAX; }
+	int CLimitsClass::get__POSIX_TRACE_SYS_MAX() { return _POSIX_TRACE_SYS_MAX; }
+	int CLimitsClass::get__POSIX_TRACE_USER_EVENT_MAX() { return _POSIX_TRACE_USER_EVENT_MAX; }
+	int CLimitsClass::get__POSIX_TTY_NAME_MAX() { return _POSIX_TTY_NAME_MAX; }
+
 
 }
--- src/api/clib/CLimitsClass.h
+++ src/api/clib/CLimitsClass.h
@@ -17,7 +17,109 @@ namespace avmshell
         CLimitsClass(VTable* cvtable);
         ~CLimitsClass();
         
+        int get_CHAR_BIT();
+        int get_CHAR_MAX();
+        int get_CHAR_MIN();
+        int get_INT_MAX();
+        int get_INT_MIN();
+        double get_LLONG_MAX();
+        double get_LLONG_MIN();
+        int get_LONG_BIT();
+        int get_LONG_MAX();
+        int get_LONG_MIN();
+        int get_SCHAR_MAX();
+        int get_SCHAR_MIN();
+        int get_SHRT_MAX();
+        int get_SHRT_MIN();
+        int get_UCHAR_MAX();
+        uint32_t get_UINT_MAX();
+        double get_ULLONG_MAX();
+        uint32_t get_ULONG_MAX();
+        int get_USHRT_MAX();
+        int get_WORD_BIT();
+
+        int get_ARG_MAX();
+        int get_ATEXIT_MAX();
+        int get_CHILD_MAX();
+        int get_LINK_MAX();
+        int get_MAX_CANON();
+        int get_MAX_INPUT();
+        int get_NAME_MAX();
+        int get_NGROUPS_MAX();
+        int get_OPEN_MAX();
         int get_PATH_MAX();
+        int get_PIPE_BUF();
+        int get_SSIZE_MAX();
+        int get_STREAM_MAX();
+        int get_TZNAME_MAX();
+
+        int get_MB_LEN_MAX();
+        int get_SIZE_MAX();
+        int get_SYMLINK_MAX();
+
+        int get_AIO_LISTIO_MAX();
+        int get_AIO_MAX();
+        int get_AIO_PRIO_DELTA_MAX();
+        int get_DELAYTIMER_MAX();
+        int get_HOST_NAME_MAX();
+        int get_LOGIN_NAME_MAX();
+        int get_MQ_OPEN_MAX();
+        int get_MQ_PRIO_MAX();
+
+        int get_PAGESIZE();
+        int get_PAGE_SIZE();
+        int get_RE_DUP_MAX();
+        int get_RTSIG_MAX();
+        int get_SEM_NSEMS_MAX();
+        int get_SEM_VALUE_MAX();
+        int get_SIGQUEUE_MAX();
+        int get_SS_REPL_MAX();
+
+        int get_SYMLOOP_MAX();
+        int get_TIMER_MAX();
+        int get_TRACE_EVENT_NAME_MAX();
+        int get_TRACE_NAME_MAX();
+        int get_TRACE_SYS_MAX();
+        int get_TRACE_USER_EVENT_MAX();
+        int get_TTY_NAME_MAX();
+
+        int get__POSIX_ARG_MAX();
+        int get__POSIX_CHILD_MAX();
+        int get__POSIX_LINK_MAX();
+        int get__POSIX_MAX_CANON();
+        int get__POSIX_MAX_INPUT();
+        int get__POSIX_NAME_MAX();
+        int get__POSIX_NGROUPS_MAX();
+        int get__POSIX_OPEN_MAX();
+        int get__POSIX_PATH_MAX();
+        int get__POSIX_PIPE_BUF();
+        int get__POSIX_SSIZE_MAX();
+        int get__POSIX_STREAM_MAX();
+        int get__POSIX_TZNAME_MAX();
+        int get__POSIX_SYMLINK_MAX();
+
+        int get__POSIX_AIO_LISTIO_MAX();
+        int get__POSIX_AIO_MAX();
+        int get__POSIX_DELAYTIMER_MAX();
+        int get__POSIX_HOST_NAME_MAX();
+        int get__POSIX_LOGIN_NAME_MAX();
+        int get__POSIX_MQ_OPEN_MAX();
+        int get__POSIX_MQ_PRIO_MAX();
+
+        int get__POSIX_RE_DUP_MAX();
+        int get__POSIX_RTSIG_MAX();
+        int get__POSIX_SEM_NSEMS_MAX();
+        int get__POSIX_SEM_VALUE_MAX();
+        int get__POSIX_SIGQUEUE_MAX();
+        int get__POSIX_SS_REPL_MAX();
+
+        int get__POSIX_SYMLOOP_MAX();
+        int get__POSIX_TIMER_MAX();
+        int get__POSIX_TRACE_EVENT_NAME_MAX();
+        int get__POSIX_TRACE_NAME_MAX();
+        int get__POSIX_TRACE_SYS_MAX();
+        int get__POSIX_TRACE_USER_EVENT_MAX();
+        int get__POSIX_TTY_NAME_MAX();
         
         DECLARE_SLOTS_CLimitsClass;
     };
