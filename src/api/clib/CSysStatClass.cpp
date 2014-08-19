7e0a4e8d1a07a40ef0f39763a88f60a9a8432277
zwetan <zwetan@gmail.com>
2014-08-19 19:02:34 +0000

    fix problems with stat struct, now use __stat64 for WIN32, and add UTF-8 support
    git-svn-id: http://dev.corsaair.com/svn/redtamarin/trunk@1538 8266667d-5eeb-4550-8638-74b212e0aa7d

--- src/api/clib/CSysStatClass.cpp
+++ src/api/clib/CSysStatClass.cpp
@@ -20,15 +20,24 @@ namespace avmshell
         
     }
 
-    int CSysStatClass::_mkdir(Stringp path, int mode)
+    int CSysStatClass::mkdir(Stringp path, int mode)
     {
         if (!path) {
             toplevel()->throwArgumentError(kNullArgumentError, "path");
         }
 
-        StUTF8String pathUTF8(path);
-        return VMPI_mkdir(pathUTF8.c_str(), mode);
+        #if AVMSYSTEM_WIN32
+            StUTF16String pathUTF16(path);
+            return VMPI_mkdir16( pathUTF16.c_str(), mode );
+        #elif
+            StUTF8String pathUTF8(path);
+            return VMPI_mkdir( pathUTF8.c_str(), mode );
+        #endif
     }
+
+
+
+
 
 
     /*static*/ int CSysStatClass::chmod(ScriptObject* self, Stringp path, int mode)
@@ -39,43 +48,13 @@ namespace avmshell
             toplevel->throwArgumentError(kNullArgumentError, "path");
         }
 
-        StUTF8String pathUTF8(path);
-        return VMPI_chmod(pathUTF8.c_str(), mode);
-    }
-
-    /*static*/ int CSysStatClass::stat(ScriptObject* self, Stringp path, CStatusObject* buf)
-    {
-        Toplevel* toplevel = self->toplevel();
-
-        if (!path) {
-            toplevel->throwArgumentError(kNullArgumentError, "path");
-        }
-
-        if (!buf) {
-            toplevel->throwArgumentError(kNullArgumentError, "buf");
-        }
-
-        StUTF8String pathUTF8(path);
-
-        struct stat statbuf;
-        int result = VMPI_stat( pathUTF8.c_str(), &statbuf );
-
-        if( result != -1 )
-        {
-            buf->set_st_dev( statbuf.st_dev );
-            buf->set_st_ino( statbuf.st_ino );
-            buf->set_st_mode( statbuf.st_mode );
-            buf->set_st_nlink( statbuf.st_nlink );
-            buf->set_st_uid( statbuf.st_uid );
-            buf->set_st_gid( statbuf.st_gid );
-            buf->set_st_rdev( statbuf.st_rdev );
-            buf->set_st_size( statbuf.st_size );
-            buf->set_st_atime( statbuf.st_atime );
-            buf->set_st_mtime( statbuf.st_mtime );
-            buf->set_st_ctime( statbuf.st_ctime );
-        }
-
-        return result;
+        #if AVMSYSTEM_WIN32
+            StUTF16String pathUTF16(path);
+            return VMPI_chmod16( pathUTF16.c_str(), mode );
+        #elif
+            StUTF8String pathUTF8(path);
+            return VMPI_chmod( pathUTF8.c_str(), mode );
+        #endif
     }
 
     /*static*/ int CSysStatClass::fstat(ScriptObject* self, int fildes, CStatusObject* buf)
@@ -98,10 +77,50 @@ namespace avmshell
             buf->set_st_uid( statbuf.st_uid );
             buf->set_st_gid( statbuf.st_gid );
             buf->set_st_rdev( statbuf.st_rdev );
-            buf->set_st_size( statbuf.st_size );
-            buf->set_st_atime( statbuf.st_atime );
-            buf->set_st_mtime( statbuf.st_mtime );
-            buf->set_st_ctime( statbuf.st_ctime );
+            buf->set_st_size( (double) statbuf.st_size );
+            buf->set_st_atime( (double) statbuf.st_atime );
+            buf->set_st_mtime( (double) statbuf.st_mtime );
+            buf->set_st_ctime( (double) statbuf.st_ctime );
+        }
+
+        return result;
+    }
+
+    /*static*/ int CSysStatClass::stat(ScriptObject* self, Stringp path, CStatusObject* buf)
+    {
+        Toplevel* toplevel = self->toplevel();
+
+        if (!path) {
+            toplevel->throwArgumentError(kNullArgumentError, "path");
+        }
+
+        if (!buf) {
+            toplevel->throwArgumentError(kNullArgumentError, "buf");
+        }
+
+        struct stat statbuf;
+
+        #if AVMSYSTEM_WIN32
+            StUTF16String pathUTF16(path);
+            int result = VMPI_stat16( pathUTF16.c_str(), &statbuf );
+        #elif
+            StUTF8String pathUTF8(path);
+            int result = VMPI_stat( pathUTF8.c_str(), &statbuf );
+        #endif
+
+        if( result != -1 )
+        {
+            buf->set_st_dev( statbuf.st_dev );
+            buf->set_st_ino( statbuf.st_ino );
+            buf->set_st_mode( statbuf.st_mode );
+            buf->set_st_nlink( statbuf.st_nlink );
+            buf->set_st_uid( statbuf.st_uid );
+            buf->set_st_gid( statbuf.st_gid );
+            buf->set_st_rdev( statbuf.st_rdev );
+            buf->set_st_size( (double) statbuf.st_size );
+            buf->set_st_atime( (double) statbuf.st_atime );
+            buf->set_st_mtime( (double) statbuf.st_mtime );
+            buf->set_st_ctime( (double) statbuf.st_ctime );
         }
 
         return result;
